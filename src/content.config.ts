7f39c82042bb0eab100c720d864b199872466055
nnjyami <y@179.jp>
2025-01-27 18:43:55 +0900

    Git非追跡ファイルの削除

--- src/content.config.ts
+++ src/content.config.ts
@@ -12,21 +12,6 @@ const blogCollection = defineCollection({
   }),
 });
 
-// NEWS
-const news2025 = defineCollection({
-  // Load Markdown and MDX files in the `src/content/blog/` directory.
-  loader: glob({ base: "./src/content/2025/", pattern: "**/*.{md,mdx}" }),
-  // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    // Transform string to Date object
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
-    number: z.string().optional(),
-  }),
-});
-
 // INTERVIEWS
 const interviews = defineCollection({
   // Load Markdown and MDX files in the `src/content/blog/` directory.
